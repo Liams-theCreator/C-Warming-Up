79c445ce2bb1bc9d0a7d83fa8c0af9e5574c6c55
Liams-theCreator <mmeellaallii1@gmail.com>
2024-06-11 20:09:22 +0100

    C02

---
+++ c02/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+char *ft_strcpy(char *dest, char *src);
+char *ft_strncpy(char *dest, char *src, unsigned int n);
+int ft_str_is_alpha(char *str);
+int ft_str_is_numeric(char *str);
+int ft_str_is_lowercase(char *str);
+int ft_str_is_uppercase(char *str);
+int ft_str_is_printable(char *str);
+char *ft_strupcase(char *str);
+char *ft_strlowcase(char *str);
+char *ft_strcapitalize(char *str);
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size);
+void ft_putstr_non_printable(char *str);
+void *ft_print_memory(void *addr, unsigned int size);
+
+
+#endif
